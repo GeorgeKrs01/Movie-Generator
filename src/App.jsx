03f023f6fec460c48740f8ec668472699e7965bd
GeorgeKrs01 <georgekrs@protonmail.com>
2024-12-08 21:03:53 +0200

    Made a basic Card just with CSS, external libraries gave me some problems. Also the width seems kind of weird

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Button from "./components/Button";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Table from "./components/Table";
-import "./App.css";
+import Card from "./components/Card";
+import "./styles/App.css";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
       <h1>Movie Generator</h1>
 
       <p>Some info</p>
+
+      <Card />
 
       <Table />
 
